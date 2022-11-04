8d1871c2b277d76947557fe5523c56d5514b19a6
devofficer <myeyesontech@gmail.com>
2022-11-04 06:20:41 -0400

    fix:action loading

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -9,7 +9,7 @@ const Modal: FC<PropsWithChildren & ExtraTWClassProps> = ({ children, className 
   const { setShowModal } = useMainAction();
 
   return (
-    <div className="fixed inset-0 w-full h-screen z-[9999] overflow-hidden bg-white bg-opacity-20 backdrop-blur-[4px]">
+    <div className="fixed inset-0 w-full h-screen z-[9998] overflow-hidden bg-white bg-opacity-20 backdrop-blur-[4px]">
       <div onClick={() => setShowModal(false)} className="relative top-24 tablet:top-16 flex justify-center max-h-[900px]">
         <div
           onClick={(e) => {
