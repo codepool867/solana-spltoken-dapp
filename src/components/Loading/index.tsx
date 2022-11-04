f8dc75b57d34231346cae1f9207f7789fc6bbfc4
devofficer <myeyesontech@gmail.com>
2022-11-04 09:30:10 -0400

    fix:action loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -5,9 +5,9 @@ import { MoonLoader } from "react-spinners";
 import { useMainAction } from "contexts";
 
 const Loading = () => {
-  const { isTXLoading } = useMainAction();
+  const { isActionLoading } = useMainAction();
 
-  return isTXLoading ? (
+  return isActionLoading ? (
     <div className="fixed inset-0 w-full h-screen z-[9999] overflow-hidden bg-black bg-opacity-50">
       <div className="relative flex justify-center items-center h-screen">
         <MoonLoader color="white" />
